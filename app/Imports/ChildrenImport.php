a5485b72d9bde44cf10a046648d97f41a9d16f4f
Hossein <hosseinyaghmaee@gmail.com>
2020-12-30 11:06:25 +0330

    Update ChildrenImport.php

--- app/Imports/ChildrenImport.php
+++ app/Imports/ChildrenImport.php
@@ -21,30 +21,51 @@ class ChildrenImport implements ToCollection, WithHeadingRow
     private function updateDatabase()
     {
         return function ($row) {
+
+            $columns = [
+                "first_name",
+                "last_name",
+                "birth_date",
+                "type",
+                "sex",
+                "emotional_text",
+                "national_code",
+                "about",
+                "priority",
+            ];
+
+            foreach ($columns as $col) {
+                if (isset($row[$col]))
+                    $data[$col] = $this->transformCols($col, $row[$col]);
+            }
+
+            $data["city_id"] = 1;
+
             Child::updateOrCreate(
                 ["national_code" => $row["national_code"]],
-                [
-                    "first_name" => $row["first_name"] ?? "",
-                    "last_name" => $row["last_name"] ?? "",
-                    "birth_date" => $this->parseGregorianDate($row["birth_date"]),
-                    "type" => $this->transformType($row["type"]),
-                    "sex" => $this->transformSex($row["sex"]),
-                    "emotional_text" => $row["emotional_text"] ?? "",
-                    "national_code" => $row["national_code"] ?? "",
-                    "about" => $row["about"] ?? "",
-                    "city_id" => 1,
-                    "priority" => $row["priority"] ?? "6",
-                ]
+                $data
             );
         };
     }
 
-    private function parseGregorianDate($jalaliDate)
+
+    public function transformCols($colIndex, $value)
+    {
+        $method = "transform_{$colIndex}";
+
+        if (method_exists($this, $method))
+            return $this->$method($value);
+
+        return $value;
+    }
+
+
+    private function transform_birth_date($jalaliDate)
     {
         return Verta::parseFormat("Y/m/d", $jalaliDate)->formatGregorian("Y-m-d");
     }
 
-    private function transformSex($sex)
+    private function transform_sex($sex)
     {
         if ($sex == "پسر" or $sex === Child::SEX_MALE)
             return Child::SEX_MALE;
@@ -52,7 +73,7 @@ class ChildrenImport implements ToCollection, WithHeadingRow
         return Child::SEX_FEMALE;
     }
 
-    private function transformType($type)
+    private function transform_type($type)
     {
         if ($type == "اکرام" or $type === Child::TYPE_EKRAM)
             return Child::TYPE_EKRAM;
